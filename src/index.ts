431bdb4dacc07582bf841b57ee521e9e1410b87d
Ben B <boucher.bn@gmail.com>
2020-08-03 18:27:38 +0200

    crud for basic users in memstore

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 
 import { UserRoutes } from './routes/user_routes'
 
@@ -10,8 +10,17 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('/users', UserRoutes)
-app.get('/*', (req, res) => res.json({ message: 'Hello dlroW' })) // Catch unhandled routes
+
+app.use((req: Request, res: Response, next: NextFunction) =>
+  res.status(404).json({ message: 'nothing here' })
+) // Catch unhandled routes
+
+app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
+  console.error(err.stack)
+  res.status(500).json({ message: 'Something broke!' })
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
